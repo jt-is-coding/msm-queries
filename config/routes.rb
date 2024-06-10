fb48328a939a9c995fff8d80246fcb8f8f76a99b
jeremybaxter <jrbaxter94@gmail.com>
2024-06-10 16:45:22 -0500

    Left off at 26:59

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "director", :action => "index"})
+  get("/directors/:the_id", { :controller => "director", :action => "show"})
 end
